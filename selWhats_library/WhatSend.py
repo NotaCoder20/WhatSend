8028ffc31beb1b6fb809e0dc89652e63563bdb14
NotaCoder20 <kevindedhia123@gmail.com>
2025-01-10 01:54:23 +0530

    Modularized and Optimized

--- selWhats_library/WhatSend.py
+++ selWhats_library/WhatSend.py
@@ -22,39 +22,55 @@ class WhatSend:
 
     def sendMessageToNewChat(self, number: str, message: str) -> bool:
 
-        new_chat_button = self.getWebElement(Elements.NEW_CHAT_ELEMENT)
-        new_chat_button.click()
-        time.sleep(0.5)
+        self.clickElement(Elements.NEW_CHAT_ELEMENT)
+        return self.sendMessage(Elements.NUMBER_BOX, Elements.NEW_CHAT, number, message, Elements.HEADER_BACK)
 
-        number_box = self.getWebElement(Elements.NUMBER_BOX)
-        number_box.click()
-        self.webDriver.sendText(number_box, number)
-        self.webDriver.pressEnter(number_box)
-        time.sleep(0.7)
+    def sendMessageToContact(self, name: str, message: str) -> bool:
 
-        try:
-            self.getWebElement(Elements.NO_CHAT_FOUND, 1)
-            self.log.warning(f"No Chat Found for: {number}")
-            self.getWebElement(Elements.HEADER_BACK).click()
+        return self.sendMessage(Elements.CONTACT_BOX, f'//*[@title="{name}"]', name, message)
+
+    def sendMessage(self, search_box_path: str, chat_path: str, name: str, message: str, press_back=None) -> bool:
+        self.typeText(search_box_path, name)
+        time.sleep(0.1)
+
+        if not self.findChat(chat_path, name):
+            if press_back:
+                self.clickElement(press_back)
             return False
-        except TimeoutError as _:
-            self.log.debug(f"Chat Found for: {number}")
-            pass
 
+        return self.sendText(message)
+
+    def sendText(self, message) -> bool:
         message_box = self.getWebElement(Elements.MESSAGE_BOX)
-        self.webDriver.sendText(message_box, message)
+        self.webDriver.sendText(Elements.MESSAGE_BOX, message)
+        self.typeText(Elements.MESSAGE_BOX, message)
         self.webDriver.pressEnter(message_box)
-        time.sleep(0.5)
-
+        time.sleep(0.1)
         return True
+
+    def typeText(self, path, text):
+        self.webDriver.sendText(path, text)
+
+    def clickElement(self, path: str, timeout=10):
+        self.webDriver.clickElement(path, timeout=timeout)
 
     def getWebElement(self, path: str, timeout=20) -> WebElement:
         return self.webDriver.wait_for_element(path, timeout=timeout)
 
+    def findChat(self, path: str, name: str) -> bool:
+        self.log.debug(path)
+        try:
+            self.clickElement(path, timeout=2)
+            self.log.debug(f'Chat Found for {name}')
+            return True
+        except TimeoutError as _:
+            self.log.warning(f"Chat Not Found for: {name}")
+            return False
+
     def testTime(self, number=1):
-        # Create a wrapper function for the method to be timed
         def send_message():
-            self.sendMessageToNewChat("9082974811", "Hey")
+            self.sendMessageToNewChat("9082974811", "Test new Chat")
+            self.sendMessageToContact("Kevin Dedhia", "Test Contact")
 
         execution_time = timeit.timeit(send_message, number=number)
         print(f"Execution time: {execution_time} seconds")
@@ -65,10 +81,11 @@ if __name__ == '__main__':
     chrome_profile_path1 = r'C:\\Users\\kevin\\AppData\\Local\\Google\\Chrome\\User Data\\'
     whatsapp = WhatSend(chrome_path1, chrome_profile_path1)
     time.sleep(5)
-    # whatsapp.sendMessageToNewChat("9082974811", "Hii")
-    # whatsapp.sendMessageToNewChat("9082974812", "Hii")
-    # whatsapp.sendMessageToNewChat("9082974811", "Hii")
-    # whatsapp.sendMessageToNewChat("9082974812", "Hii")
-    # whatsapp.sendMessageToNewChat("9082974811", "Hii")
-    # whatsapp.sendMessageToNewChat("9082974812", "Hii")
-    whatsapp.testTime(10)
+    for i in range(10):
+        whatsapp.sendMessageToNewChat("9082974811", "Test new Chat")
+        whatsapp.sendMessageToContact("Sbham", "Test Contact")
+        whatsapp.sendMessageToNewChat("9082974812", "Test new Chat")
+        whatsapp.sendMessageToContact("Shubham", "Test Contact")
+
+
+    # whatsapp.testTime(10)
